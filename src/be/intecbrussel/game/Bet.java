6054c2599e1e7e3279c46e14ad2b4c2387daf94f
AxelB0 <axel.boonen.mail@gmail.com>
2022-04-29 11:36:07 +0200

    commit

--- src/be/intecbrussel/game/Bet.java
+++ src/be/intecbrussel/game/Bet.java
@@ -2,4 +2,10 @@ package be.intecbrussel.game;
 
 public class Bet {
 
+    private final double WINRATE = 2.5;
+    private double amountBet;
+
+
 }
+
+
--- src/be/intecbrussel/util/CardPoint.java
+++ src/be/intecbrussel/util/CardPoint.java
@@ -1,4 +1,23 @@
 package be.intecbrussel.util;
 
+import be.intecbrussel.cards.Card;
+
 public class CardPoint {
+    public int getScore(Card card){
+        String value = card.getValue();
+        if (value.equals("K")|| value.equals("Q")||value.equals("J")){
+            return 10;
+        } else if(value.equals("A")){
+            return 11;
+        } else {
+            return Integer.valueOf(value);
+        }
+    }
+
+
+
+
+
+
+
 }
