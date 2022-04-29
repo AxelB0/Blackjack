78ba9c71c548e49b98d44cc54ea3180dd856a046
AxelB0 <axel.boonen.mail@gmail.com>
2022-04-29 11:48:08 +0200

    deel 1

--- src/be/intecbrussel/game/Bet.java
+++ src/be/intecbrussel/game/Bet.java
@@ -5,6 +5,17 @@ public class Bet {
     private final double WINRATE = 2.5;
     private double amountBet;
 
+    public Bet(double amount) {
+        setAmountBet(amount);
+    }
+
+    public void setAmountBet(double amountBet) {
+        this.amountBet = amountBet;
+    }
+
+    public double winBet() {
+        return this.amountBet *= WINRATE;
+    }
 
 }
 
---
+++ src/be/intecbrussel/game/Table.java
@@ -0,0 +1,7 @@
+package be.intecbrussel.game;
+
+public class Table {
+    public static void main(String[] args) {
+
+    }
+}
